9d6572e4f9aae8462b6cc4513a6568e396f91c0f
Josh Hayes <joshjchayes@gmail.com>
2021-01-19 16:27:59 +0000

    Revert "Revert "v2.1.4""
    This reverts commit 35e14983770afc7aeda552f71a3fb3c3764a44bb.

--- transitfit/__init__.py
+++ transitfit/__init__.py
@@ -4,7 +4,7 @@ TransitFit package
 This package is designed to fit transit light curves using BATMAN
 '''
 name = 'transitfit'
-__version__ = '2.1.4_dev'
+__version__ = '2.1.4'
 
 from .retriever import Retriever
 from .priorinfo import PriorInfo
