a8e193ce7b016ae2e8db1280862658df0d08b18d
Josh <joshjchayes@gmail.com>
2021-06-07 12:06:34 +0100

    Update version number to 2.3.0

--- transitfit/__init__.py
+++ transitfit/__init__.py
@@ -4,7 +4,7 @@ TransitFit package
 This package is designed to fit transit light curves using BATMAN
 '''
 name = 'transitfit'
-__version__ = '2.3.0dev'
+__version__ = '2.3.0'
 
 from .retriever import Retriever
 from .priorinfo import PriorInfo
